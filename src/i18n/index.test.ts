b910a8ab055a4cc3de15b3f87e3e7214907dab14
xentriom <scobbleqt@gmail.com>
2026-04-20 19:36:25 -0400

    Update index.test.ts

--- src/i18n/index.test.ts
+++ src/i18n/index.test.ts
@@ -1,9 +1,9 @@
 import { describe, it, expect } from "vitest";
-import type { Locale } from "#/types/locale.js";
+import { Language } from "#/types/locale.js";
 import { t } from "./index.js";
 
 describe("i18n", () => {
   it("should get a locale", () => {
-    expect(t("de-de" as unknown as Locale, "lang")).toBe("Deutsch");
+    expect(t(Language.DE_DE, "lang")).toBe("Deutsch");
   });
 });

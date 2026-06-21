8cb7889c5ae20eca584c8d50292a09a7defd80cd
xentriom <scobbleqt@gmail.com>
2026-06-20 22:38:11 -0400

    feat(cmd): basic translations for cc

--- src/commands/contingency/slash.ts
+++ src/commands/contingency/slash.ts
@@ -1,11 +1,20 @@
 import { SlashCommandBuilder, type ChatInputCommandInteraction } from "discord.js";
+import { dtx } from "#/i18n/index.ts";
 
 export default {
   cooldown: 5,
   data: new SlashCommandBuilder()
     .setName("contingency")
+    .setNameLocalizations(dtx("command.contingency.name"))
     .setDescription("About the bot")
-    .addSubcommand((subcommand) => subcommand.setName("contract").setDescription("About the bot")),
+    .setDescriptionLocalizations(dtx("command.contingency.description"))
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("contract")
+        .setNameLocalizations(dtx("command.contingency.subcommands.contract.name"))
+        .setDescription("About the bot")
+        .setDescriptionLocalizations(dtx("command.contingency.subcommands.contract.description")),
+    ),
   execute: async (interaction: ChatInputCommandInteraction) => {
     await interaction.reply("About the bot!");
   },
